3560f0cc3ee476ef648f2e74eb336b0d1ca09a83
evgen <admin@192.168.60.78>
2017-08-03 17:26:38 +0300

    setting spring

---
+++ OnlineShop/src/com/myproject/controllers/Test.java
@@ -0,0 +1,22 @@
+package com.myproject.controllers;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+	
+	private String mes;
+	
+	public void setMes(String mes) {
+		this.mes = mes;
+	}
+	
+	public static void main(String[] args){
+		@SuppressWarnings("resource")
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-dao.xml");
+		Test test = (Test) context.getBean("test");
+		System.out.println(test.mes);
+		
+	}
+
+}
